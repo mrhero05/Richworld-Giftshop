ecdbc8fefb3f8a5092985c7b35473c69ba54db9c
Ogie Sanchez <ogiesanchez.hs@gmail.com>
2021-05-13 16:22:15 +0800

    index
    lets try

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>Richworld Giftshops</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <p>LOGIN</p>
+            <p>Username</p>
+            <input type="text" placeholder="Username">
+            <p>Password</p>
+            <input type="password" placeholder="Password">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
