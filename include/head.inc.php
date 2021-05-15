6ecc5b5b1656d72b9d25c780f0bfa3b48b727717
Ogie Sanchez <herofaulkerson@gmail.com>
2021-05-15 18:52:04 +0800

    added login function in php

--- include/head.inc.php
+++ include/head.inc.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="des.css?v=<?php echo time();?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    
     <title>Richworld Giftshops</title>
 </head>
 <body>
