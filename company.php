8055b309cd2fcc980a9435a3c7caabd14b25fec3
lumeriothompie <thompie.lumerio27@gmail.com>
2021-05-24 01:41:40 +0800

    updated

---
+++ company.php
@@ -0,0 +1,20 @@
+<?php
+include 'include/head.inc.php';
+include 'include/nav.inc.php';
+?>
+
+<div class="col-10">
+    <h1>Company Structure</h1>
+    <button type="button">Add department</button>
+        <div class="row">
+            <div class="col-12">
+                <p class="company-container"></p>
+                    <div class="admin-container">
+                        <p>ADMIN</p>
+                    </div>
+            </div>
+        </div>
+</div>
+
+
+
