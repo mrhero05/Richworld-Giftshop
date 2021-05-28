20eb11a7513351085f37a9852c3edf38d5318e91
Ogie Sanchez <herofaulkerson@gmail.com>
2021-05-29 02:35:41 +0800

    working update button

--- include/search.inc.php
+++ include/search.inc.php
@@ -9,35 +9,46 @@ include 'dbc.inc.php';
         }else{
             $gotSearch = $_POST["getSearch"];
             $gotCheck = $_POST["getCheck"];
-            $sql = "SELECT * from account where ".$gotCheck." like '%$gotSearch%'";
+            $sql = "SELECT * from employee where ".$gotCheck." like '%$gotSearch%'";
             $view = mysqli_query($conn,$sql);
             if(mysqli_num_rows($view) > 0){
-                echo '<table class="table table-hover">
+                echo '<table class="table table-hover" id="viewTableSearch">
                     <thead class="table-dark">
                         <tr>
                         <th scope="col">ID</th>
                         <th scope="col">First name</th>
                         <th scope="col">Last name</th>
-                        <th scope="col">Gender</th>
-                        <th scope="col">Civil Status</th>
-                        <th scope="col">Contact #</th>
-                        <th scope="col">Email address</th>
+                        <th scope="col">Age</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Gender #</th>
+                        <th scope="col">Civil status</th>
+                        <th scope="col">Contact</th>
                         <th scope="col">Job title</th>
-                        <th scope="col">Employment</th>
+                        <th scope="col">Employment type</th>
+                        <th scope="col">Probation period</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Employee contract</th>
+                        <th scope="col">Option</th>
                         </tr>
                     </thead>';
                 while($row = mysqli_fetch_assoc($view)){
                     
 
-                    echo '<tbody><tr><td>'.$row["acc_id"].'</td>
-                        <td>'.$row["firstname"].'</td>
-                        <td>'.$row["lastname"].'</td>
-                        <td>'.$row["contact"].'</td>
-                        <td>'.$row["contact"].'</td>
-                        <td>'.$row["contact"].'</td>
-                        <td>'.$row["contact"].'</td>
-                        <td>'.$row["contact"].'</td>
-                        <td>'.$row["contact"].'</td>
+                    echo '<tbody><tr>
+                        <td>'.$row["emp_id"].'</td>
+                        <td>'.$row["emp_fname"].'</td>
+                        <td>'.$row["emp_lname"].'</td>
+                        <td>'.$row["emp_age"].'</td>
+                        <td>'.$row["emp_email"].'</td>
+                        <td>'.$row["emp_gender"].'</td>
+                        <td>'.$row["emp_civil"].'</td>
+                        <td>'.$row["emp_contact"].'</td>
+                        <td>'.$row["emp_jobTitle"].'</td>
+                        <td>'.$row["emp_empType"].'</td>
+                        <td>'.$row["emp_probPeriod"].'</td>
+                        <td>'.$row["emp_perAdd"].'</td>
+                        <td>'.$row["emp_empContract"].'</td>
+                        <td> <button type="button" class="updateGo" data-toggle="modal" data-target="#updateModal" onclick="dataUpdateSearch()">Update</button></td>
                         </tr>';
                     
                 }
