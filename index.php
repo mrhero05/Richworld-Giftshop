d83353f933e2f8b671bfeb7fc674d15cb93c91fd
Ogie Sanchez <herofaulkerson@gmail.com>
2021-05-28 20:28:53 +0800

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
 include 'include/head.inc.php';
-include 'include/nav.inc.php';
 ?>
 
 </body>
 </html>
+<nav class="navbar navbar-expand-lg navbar-light" id="nav_index">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ICON</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        
+            <li class="nav-item">
+            <a class="nav-link active"aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Download</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" style="margin-right:20px">About</a>
+            </li>
+            <li class="nav-item">
+            <a href="#"><button type="button" class="nav-login">Login</button></a>
+            </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<section id="nav-body">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            
+        </div>
+    </div>
+</div>
+</section>
