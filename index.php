7c5bd2dbfb5a463757d3054f5b321b2644d14a06
kenenth pole <polekenneth11@gmail.com>
2021-06-02 23:28:38 +0800

    Animations added(Not finished)

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'include/head.inc.php';
 </html>
 <nav class="navbar navbar-expand-lg navbar-light" id="nav_index">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">logo</a>
+    <a class="navbar-brand" href="index.php"><p>R<span>G</span></p></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -30,6 +30,11 @@ include 'include/head.inc.php';
   </div>
 </nav>
 <section id="nav-body">
+<div data-aos="fade-up"
+    data-aos-delay="50"
+    data-aos-duration="800"
+    data-aos-easing="ease-in"
+>
 <div class="container">
     <div class="row">
       <!-- for introduction part -->
@@ -46,11 +51,12 @@ include 'include/head.inc.php';
           </div>
         </div>
     </div>
+    </div>
 
 </div>
+
 <div class="container-fluid">
   <div class="row">
-    <div class="col-lg-12 p-0">
       <div class="bgwave-red">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FD5757" fill-opacity="1" d="M0,128L40,128C80,128,160,128,240,154.7C320,181,400,235,480,245.3C560,256,640,224,720,181.3C800,139,880,85,960,80C1040,75,1120,117,1200,133.3C1280,149,1360,139,1400,133.3L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>   
         </svg>
@@ -58,28 +64,47 @@ include 'include/head.inc.php';
         <div class="container-fluid features">
           <div class="row">
             <div class="col-lg-4">
+            <div data-aos="slide-up"
+                  data-aos-delay="50"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+              >
               <div class="easy">
               <img src="img/index_icon/easy.svg">
               <h2>Easy to use</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit pellentesque. Ultrices gravida dictum fusce ut placerat orci. Egestas sed sed risus pretium quam vulputate. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. </p>
               </div>
+              </div>
             </div>
             <div class="col-lg-4">
+            <div data-aos="slide-up"
+                  data-aos-delay="50"
+                  data-aos-duration="1100"
+                  data-aos-easing="ease-in-out"
+              >
               <div class="recruit">
                 <img src="img/index_icon/recruitment.svg">
                 <h2>recruitment</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit pellentesque. Ultrices gravida dictum fusce ut placerat orci. Egestas sed sed risus pretium quam vulputate. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. </p>
                 </div>
+                </div>
             </div>
             <div class="col-lg-4">
+            <div data-aos="slide-up"
+                  data-aos-delay="50"
+                  data-aos-duration="1200"
+                  data-aos-easing="ease-in-out"
+              >
               <div class="security">
                 <img src="img/index_icon/security.svg">
                 <h2>Security</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit pellentesque. Ultrices gravida dictum fusce ut placerat orci. Egestas sed sed risus pretium quam vulputate. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. </p>
                 </div>
             </div>
+            </div>
           </div>
         </div>  
+      </div>
       </div>
 </div>
 </section>
@@ -90,17 +115,30 @@ include 'include/head.inc.php';
       <div class="row">
         <div class="col-lg-5">
           <div class="d-img">
-          <img src="img/index_icon/android.png"></div>
+          <div data-aos="slide-right"
+                data-aos-delay="40"
+                data-aos-duration="1100"
+                data-aos-easing="ease-in-out"
+            >
+          <img src="img/index_icon/android.png">
+          </div>
+          </div>
         </div>
         <div class="col-lg-7">
+        <div data-aos="slide-left"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              data-aos-easing="ease-in-out"
+          >
           <div class="d-title">
           <h1>Richworld Giftshop App</h1>
           <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit pellentesque. Ultrices gravida dictum fusce ut placerat orci. Egestas sed sed risus pretium quam vulputate. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. </h5>
           <button type="button">Download App</button>
           </div>
         </div>
+        </div>
       </div>
-    </div>
+  </div>
   </div>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2f2e41" fill-opacity="1" d="M0,128L26.7,133.3C53.3,139,107,149,160,133.3C213.3,117,267,75,320,53.3C373.3,32,427,32,480,64C533.3,96,587,160,640,160C693.3,160,747,96,800,90.7C853.3,85,907,139,960,165.3C1013.3,192,1067,192,1120,202.7C1173.3,213,1227,235,1280,218.7C1333.3,203,1387,149,1413,122.7L1440,96L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path></svg>
 </section>
@@ -121,3 +159,8 @@ include 'include/head.inc.php';
   </div>
 </div>
 </section>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+AOS.init();
+</script>
