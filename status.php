14127758a4dd864c2c3f1f4067c976009265ac18
kenenth pole <polekenneth11@gmail.com>
2021-09-03 18:41:19 +0800

    step progress added

---
+++ status.php
@@ -0,0 +1,60 @@
+<?php
+include 'include/head.inc.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="step-header">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <h2>Application Status</h2>
+                <p>Your latest progress will be shown here</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="step-progress">
+            <div class="col-sm-12 col-md-6 col-lg-6 p-0">
+                <ul>
+                    <li>
+                        <h5><img src="img/index_icon/Circle.svg" alt="">Resume submitted</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circle.svg" alt="">Resume reviewed</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Schedule for interview</h5>
+                        <p>Wait for HR officer to schedule your interview. You will notified via email or SMS</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Initial interview</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Exam (Written/Medical)</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Final interview</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Contract Signing</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                    <li>
+                        <h5><img src="img/index_icon/Circled.svg" alt="">Deployment</h5>
+                        <p>12/21/12</p>
+                        <p>11:11PM</p>
+                    </li>
+                </ul>
+
+        </div>
+    </div>
+</div>
